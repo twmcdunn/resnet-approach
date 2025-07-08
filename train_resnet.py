9feeec08ad32cbb99d43a9cd0b59457bd99781a2
twmcdunn <twcdunn@gmail.com>
2025-07-08 15:47:13 -0500

    upodate

--- train_resnet.py
+++ train_resnet.py
@@ -20,7 +20,6 @@ train_transforms = transforms.Compose([
     transforms.RandomHorizontalFlip(0.5),
     transforms.RandomRotation(10),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-    transforms.GaussianBlur(blur_limit=3),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
